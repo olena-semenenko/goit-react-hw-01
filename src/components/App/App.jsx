fbad0ca758c3b1818fc417b076b72b60b70ccdd8
olena-semenenko <elen.kinakh@gmail.com>
2024-03-10 12:01:18 +0100

    fixed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import React from 'react'
+import { useState } from 'react';
+import React from 'react';
 
-import Profile from '../Profile/Profile'
-import FriendList from '../FriendList/FriendList'
-import TransactionHistory from '../TransactionHistory/TransactionHistory'
+import Profile from '../Profile/Profile';
+import FriendList from '../FriendList/FriendList';
+import TransactionHistory from '../TransactionHistory/TransactionHistory';
 
-import user from '/src/data/User.json'
-import friends from '/src/data/Friends.json'
-import transaction from '/src/data/Transaction.json'
-
-const { username, tag, location, avatar, stats } = user
+import user from '/src/data/User.json';
+import friends from '/src/data/Friends.json';
+import transaction from '/src/data/Transaction.json';
 
 const App = () => {
   return (
     <div>
       <Profile
-        avatar={avatar}
-        username={username}
-        tag={tag}
-        location={location}
-        followers={stats.followers}
-        views={stats.views}
-        likes={stats.likes}></Profile>
-      
-      <FriendList
-        friends={friends}></FriendList>
-      
+        avatar={user.avatar}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        stats={user.stats}
+      ></Profile>
+
+      <FriendList friends={friends}></FriendList>
+
       <TransactionHistory items={transaction}></TransactionHistory>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,41 +1,31 @@
+import css from './Profile.module.css';
 
-import css from './Profile.module.css'
-
-
-
- const Profile = ({username, tag, location, avatar, followers, views, likes}) => {
+const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
-      <div className={css.profile_card}>
-        <div className={css.profile_info}>
-              <img className={css.profile_img}
-                  src={avatar}
-                  alt="User avatar"
-              />
-              <p className={css.profile_name}>{username}</p>
-              <p>@{tag}</p>
-              <p>{location}</p>
-          </div>
-
-          <ul className={css.profile_stats}>
-              <li className={css.stats}>
-                  <span>Followers</span>
-                  <span className={css.stats_info}>{followers}</span>
-              </li>
-              <li className={css.stats}>
-                  <span>Views</span>
-                  <span className={css.stats_info}>{views}</span>
-              </li>
-              <li className={css.stats}>
-                  <span>Likes</span>
-                  <span className={css.stats_info}>{likes}</span>
-              </li>
-          </ul>
+    <div className={css.profile_card}>
+      <div className={css.profile_info}>
+        <img className={css.profile_img} src={avatar} alt="User avatar" />
+        <p className={css.profile_name}>{username}</p>
+        <p>@{tag}</p>
+        <p>{location}</p>
       </div>
 
-  )
-}
+      <ul className={css.profile_stats}>
+        <li className={css.stats}>
+          <span>Followers</span>
+          <span className={css.stats_info}>{stats.followers}</span>
+        </li>
+        <li className={css.stats}>
+          <span>Views</span>
+          <span className={css.stats_info}>{stats.views}</span>
+        </li>
+        <li className={css.stats}>
+          <span>Likes</span>
+          <span className={css.stats_info}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-
-
-
-export default Profile
+export default Profile;
